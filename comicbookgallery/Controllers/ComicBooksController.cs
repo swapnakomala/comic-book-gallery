b7e904db6a40009c3841859194a9839a8da90a2c
swapna <gannamaneni.swapna@gmail.com>
2018-11-08 18:25:32 -0500

    First commit

---
+++ comicbookgallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ActionResult Detail()
+        {
+            ViewBag.seriesTitle = "The Spider-man";
+            ViewBag.issueNumber = 700;
+            ViewBag.description = "<p>The Poem Below </P> ";
+            ViewBag.artists = new string[]
+            {
+            "Tokyo to Hokkaido",
+            "and an expensive",
+            "Taxi to Niseko Tokyo",
+            "to the Sapporo, now"
+            };
+            return View();
+
+        }
+
+    }
+}

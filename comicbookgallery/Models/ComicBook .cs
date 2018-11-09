09001948dc4495188a497d05daee7550d89e2fe7
swapna <gannamaneni.swapna@gmail.com>
2018-11-08 19:00:00 -0500

    Added ComicBook files

---
+++ comicbookgallery/Models/ComicBook .cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHTML { get; set; }
+        public Artist[] Artist { get; set; }
+        public bool Favorite { get; set; }
+
+        public string DisplatText
+        {
+            get
+            {
+                return SeriesTitle + "#" + IssueNumber;
+            }
+        }
+        //series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "_").ToLower() + "_" + IssueNumber + ".jpg";
+            }
+        }
+    }
+}
